5e2a4ae9c7a8478f92929e0083ab4ecaed7ed273
Antoniojld <antoniojoselopezdiaz.0404@gmail.com>
2024-11-18 20:47:45 -0400

    agregando todos los hack

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,20 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
-    return result  
+    output = []
+    for char in result:
+        if char == 'f':
+            output.append('F')
+        elif char == 'o':
+            output.append('0')
+        elif char == 'z':
+            output.append('Z')
+        elif char == 'i':
+            output.append('1')
+        elif char == 'm':
+            output.append('M')
+        elif char == 'a':
+            output.append('@')
+        elif char == 'n':
+            output.append('N')
+    return output
--- hack_9.py
+++ hack_9.py
@@ -3,6 +3,11 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 """
 
 def fn_hack_9():
-    result = [1,2,3]
-    #...
-    return result  
+    result = [1, 2, 3]
+    output = []
+    i = 0
+    while i < len(result):
+        output.append(result[i])
+        output.append('@')
+        i += 1
+    return output
